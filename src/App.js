2e2405d3ced08c0c521869797a2aedfc78b379ba
Monojit Mondal <mondalmonojit1696@gmail.com>
2023-01-17 16:54:36 +0530

    App: Complete folder structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Mainroute from './routes/Mainroute';
+import Footer from './components/footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Mainroute />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
