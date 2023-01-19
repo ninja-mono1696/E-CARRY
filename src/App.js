6d29d110cc42ce92cdf7d4555f79c8dcfe4664c0
niraj gope <niraj55gope@gmail.com>
2023-01-19 20:05:05 +0530

    day-1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Mainroute from './routes/Mainroute';
 import Footer from './components/footer/Footer';
+import Login from './components/login/Login';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
       <Navbar />
       <Mainroute />
       <Footer />
+      <Login/>
+      
     </div>
   );
 }
