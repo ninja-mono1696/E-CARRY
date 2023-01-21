fcb4e64af5391b9aad5530ff140f1b638e3a924a
niraj gope <niraj55gope@gmail.com>
2023-01-21 20:19:29 +0530

    day-3

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Mainroute from "./routes/Mainroute";
 import Footer from "./components/footer/Footer";
-import Login from "./components/login/Login";
+
+import Log from "./components/login/Log";
+import {SimpleSidebar} from "./pages/accountPage/Account";
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
-      {/* <Mainroute /> */}
+      <Mainroute />
       {/* <Footer /> */}
-      <Login/>
+   
+      {/* <Log/> */}
+      {/* <SimpleSidebar/> */}
       
     </div>
   );
