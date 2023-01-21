6af12f4d35222bb5252645496807557effdf8784
Kavita mali <kavita13feb@gmail.com>
2023-01-21 16:52:17 +0530

    single product and cart page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 
-// import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Mainroute from './routes/Mainroute';
 import Footer from './components/footer/Footer';
-import ProductPage from './pages/productPage/ProductPage';
-// import SingleProductPage from './pages/productPage/singleProductPage/SingleProductPage';
-
-import Login from './components/login/Login';
-import "./App.css";
-
-import { SingleProductPage } from './pages/productPage/singleProductPage/SingleProductPage';
-import Cart from './pages/cartPage/Cart';
-
 
 
 function App() {
